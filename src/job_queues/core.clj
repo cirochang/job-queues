3e2058cef54651c9298a1d8b06cd9b40bcf9fa6b
Ciro Chang <cirochang@live.com>
2018-02-18 20:14:31 -0300

    add tests for control namespace

--- src/job_queues/core.clj
+++ src/job_queues/core.clj
@@ -1,4 +1,5 @@
 (ns job-queues.core
+  (:gen-class)
   (:require [clojure.data.json :as json]
             [job-queues.utils :as utils]))
 
@@ -15,7 +16,7 @@
     (split-input input entity)))
 
 (defn order-jobs-by-urgent
-  "Order jobs elements by urgent true"
+  "Order jobs elements by urgent true"; doc: Put urgent jobs in the beggining of sequence
   [jobs]
   (let [splited-jobs (group-by #(get % "urgent") jobs)]
     (concat (get splited-jobs true)
@@ -26,12 +27,12 @@
   [agent-id agents]
   (first (filter #(= (get % "id") agent-id) agents)))
 
-(defn get-jobs-by-type
+(defn get-jobs-by-type ;; change type to skillset 
   "Get jobs by skillset"
   [jobs skillset]
   (filter #(>= (.indexOf skillset (get % "type")) 0) jobs))
 
-(defn get-best-jobs-by-agent
+(defn get-best-jobs-by-agent ;; change this function name
   "Seach the bests jobs for some agent"
   [jobs agent]
   (let [primary_skillset (get agent "primary_skillset")
@@ -40,26 +41,26 @@
         secondary-bests-jobs (get-jobs-by-type jobs secondary_skillset)]
     (distinct (concat primary-bests-jobs secondary-bests-jobs))))
 
-(defn get-selected-job
-  "Select the correct job"
+(defn remove-jobs-already-assigned
+  "Remove jobs that already assigned"
   [best-jobs jobs_assigned]
   (let [id_assigned (map #(get (get % "job_assigned") "job_id") jobs_assigned)]
-    (first (remove #(>= (.indexOf id_assigned (get % "id")) 0) best-jobs))))
+    (remove #(>= (.indexOf id_assigned (get % "id")) 0) best-jobs)))
 
 (defn assign-jobs
   "Assign Jobs to Agent"
   [job_request agents jobs jobs_assigned]
   (if (seq job_request)
     (let [agent (get-agent-by-id (get (first job_request) "agent_id") agents)
-          best-jobs (get-best-jobs-by-agent jobs agent)
-          selected-job (get-selected-job best-jobs jobs_assigned)
+          best-jobs (get-best-jobs-by-agent jobs agent) ;; change to possible jobs
+          selected-job (first (remove-jobs-already-assigned best-jobs jobs_assigned)) ;; change to best-job
           new_job_assigned {"job_assigned" {"job_id" (get selected-job "id"), "agent_id" (get agent "id")}}]
       (assign-jobs (rest job_request) agents jobs (conj jobs_assigned new_job_assigned) ))
     jobs_assigned))
 
 
 (defn -main
-  "Read from STDIN"
+  "Read json from stdin and " ;; change this doc
   []
   (let [input (json/read-str (utils/stream-to-str *in*))
         jobs (order-jobs-by-urgent (parse-input input "new_job"))
