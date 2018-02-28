f78321ad8b9b3a82c57c9e338da068ca0c5759fc
Ciro Chang <cirochang@live.com>
2018-02-27 23:08:20 -0300

    add ring dependency

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "An exercise of functional programming provided by Nubank."
   :url "https://gitlab.com/cirochang/job-queues/tree/organizing-repository"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [ring "1.4.0"]]
   :main ^:skip-aot job-queues.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
