e772a7d461b98e102094850d258cb4ea1b140901
Ciro Chang <cirochang@live.com>
2018-03-04 03:22:30 -0300

    lint

--- src/job_queues/controller.clj
+++ src/job_queues/controller.clj
@@ -31,9 +31,9 @@
       (status (response (str (.getMessage e))) 400))))
 
 (defn assign-job
-  "Complete the job already assigned by agent if this job exists.
-   Assign an agent-id to a best job match if this match exists.
-   And return the id of this job or indicate the lack of one."
+  "Complete the last job assigned by agent if this job exists.
+  Assign an agent-id to a best job match if this match exists.
+  And return the id of this job or indicate the lack of one (null)."
   [request-job]
   (try
     (do
@@ -44,8 +44,7 @@
                 secondary-job (database/get-best-job-by-skillsets (:secondary_skillset agent))
                 best-job (first (concat primary-job secondary-job))]
             (database/complete-job-by-agent (:id agent))
-            (if best-job
-              (database/assign-job-to-agent (:id best-job) (:id agent)))
+            (if best-job (database/assign-job-to-agent (:id best-job) (:id agent)))
             (response {"job_id" (:id best-job) "agent_id" (:id agent)}))
           (status (response "Agent not found") 404))))
     (catch java.lang.RuntimeException e
@@ -53,15 +52,15 @@
 
 (defn get-queue-state
   "Output a breakdown of the job queue.
-   Consisting of all being done, completed, and waiting jobs."
-   []
-   (let [jobs (database/get-all-jobs)
-         jobs-grouped (group-by :status jobs)]
-     (response jobs-grouped)))
+  Consisting of all being done, completed, and waiting jobs."
+  []
+  (let [jobs (database/get-all-jobs)
+        jobs-grouped (group-by :status jobs)]
+    (response jobs-grouped)))
 
 (defn get-agent-stats
   "Given an agent, output how many jobs of each type this agent has performed."
   [agent-id]
   (let [jobs (database/get-all-jobs-assigned-by-agent agent-id)
         num-jobs-by-type (frequencies (map :type jobs))]
-        (response num-jobs-by-type)))
+    (response num-jobs-by-type)))
